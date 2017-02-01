59059e2ca87a93034a55934cb7d4546711c6209d
Lar <Lar@Lars-MacBook-Pro.local>
2017-02-01 20:56:57 +0000

    Now uses PDO statements

--- php/login.php
+++ php/login.php
@@ -4,44 +4,31 @@
 
    require_once __DIR__ . '/db_connect.php';
 
-   // connecting to db
-   $db = new DB_CONNECT();
-
-   // Checks for required field
    if(isset($_POST['username']) && isset($_POST['password']))
    {
       $username = $_POST['username'];
       $password = $_POST['password'];
 
-      $result = mysql_query("SELECT * FROM user WHERE username = '$username' AND password = $password");
+      $stmt = $pdo->prepare('SELECT * FROM user WHERE username = ? AND password = ?');
+      $stmt->execute([$username, $password]);
+      $result = $stmt->fetch();
 
       if (!empty($result)) {
+         $response["success"] = 1;
+         $response["status"] = 200;
+         $response["message"] = "User found";
 
-         // If result has returned a row
-         if (mysql_num_rows($result) > 0) {
-            $response["success"] = 1;
-            $response["status"] = 200;
-            $response["message"] = "User found";
+         // returns response in JSON format
+         echo json_encode($response);
 
-            // returns response in JSON format
-            echo json_encode($response);
-         }
-         else {
-
-          $response["success"] = 0;
-          $response["status"] = 404;
-          $response["message"] = "User not found";
-
-          echo json_encode($response);
       }
-    }
-    else {
+      else {
+         $response["success"] = 1;
+         $response["status"] = 404;
+         $response["message"] = "User not found";
 
-      $response["success"] = 0;
-      $response["status"] = 404;
-      $response["message"] = "User not found";
-
-      echo json_encode($response);
+         // returns response in JSON format
+         echo json_encode($response);
+      }
    }
-}
 ?>
